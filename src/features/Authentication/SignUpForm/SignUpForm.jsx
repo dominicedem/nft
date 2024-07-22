6ffcc5b475667f56983dc6f4dcb82b45d184703a
dominicedem <githubdominic@gmail.com>
2024-07-22 21:57:06 +0100

    logo and name remaing

--- src/features/Authentication/SignUpForm/SignUpForm.jsx
+++ src/features/Authentication/SignUpForm/SignUpForm.jsx
@@ -140,7 +140,7 @@ const LoadingBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   width: 100%;
